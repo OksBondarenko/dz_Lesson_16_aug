8eaa706a7828da24eb99ff580928c12124940043
Oksana bondarenko <roksanapopova@gmail.com>
2022-08-22 23:15:53 +0200

    Task 27

---
+++ задача27/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.(123 -> 6)
+
+int SumDigit(int num)
+{
+    int sum = 0;
+    for(; num > 0; num /= 10)
+        sum += num % 10;
+    return sum;
+}
+
+Console.Write("Введите число: ");
+int digit = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(SumDigit(digit));
